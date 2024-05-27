f245c378b62625a359c36ebe77c797bf8deb91f6
SS11146111 <sahasangita59@gmail.com>
2024-05-27 23:47:19 +0530

    delete table row

--- js/viewRecords.js
+++ js/viewRecords.js
@@ -7,10 +7,24 @@ var tableData = data.map(record => (
         <td>${record.sid}</td>
         <td>${record.smail}</td>
         <td>${record.snumber}</td>
-        <td><button id="editBtn">Edit</button></td>
-        <td><button id="deleteBtn">Delete</button></td>
+        <td><button class="editBtn">Edit</button></td>
+        <td><button class="deleteBtn">Delete</button></td>
     </tr>
     `
   )).join('');
 
   document.getElementById("tableBody").innerHTML = tableData;
+
+  document.getElementById("tableBody").addEventListener("click",
+
+  function(e){
+   
+    const data = e.target;
+    if(data.classList[0] === "deleteBtn"){
+        const parent = data.parentNode.parentNode;
+        //const text=parent.childNodes[2].innerHTML;
+        //alert(text);
+        parent.remove();
+    }
+  }
+  )
